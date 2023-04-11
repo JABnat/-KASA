54c7cda7688b26eb5a63eba0eb9426ee7bec139c
NJ <109919004+JABnat@users.noreply.github.com>
2023-04-11 21:50:10 +0000

    refining collapse

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,6 +7,7 @@ export default function About() {
         <div>
             <Navigator />
             <Banner />
+            <div className='container-collapse'>
             <Collapse 
                 title="Fiablité"
                 description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
@@ -23,6 +24,7 @@ export default function About() {
                 title="Sécurité"
                 description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
             />
+            </div>
         </div>
     )
 };
