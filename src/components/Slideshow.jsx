c01185ba8adae6403c974b56945a74ec37f1e73f
NJ <109919004+JABnat@users.noreply.github.com>
2023-04-16 13:06:00 +0000

    notes nad formatting

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -4,21 +4,21 @@ import right from "../assets/images/Vector-right.svg";
 import '../styles/Slideshow.css'
 
 export default function Slideshow({ slides }) {
-	const [current, setCurrent] = useState(0); //je définie l'index du premier slide à 0
-	const length = slides.length; // longueur du tableau de slides
+	const [current, setCurrent] = useState(0); //Set the index of the first slide to 0
+	const length = slides.length; // length of the array
 
 	const nextSlide = () => {
-		setCurrent(current === length - 1 ? 0 : current + 1); // on repart au premier slide quand on arrive au dernier
+		setCurrent(current === length - 1 ? 0 : current + 1); // go back to the first slide when we reach the last one
 	};
 	const prevSlide = () => {
-		setCurrent(current === 0 ? length - 1 : current - 1); // on repart au dernier slide quand on est au premier
+		setCurrent(current === 0 ? length - 1 : current - 1); // go back to the last slide when arriving at the first pic
 	};
 
 	return (
 		<section className="slideshow-container">
 			{slides.map((slide, index) => (
 				<div
-					key={index} // mise en place du slider avec affichage conditionnel et opacity=1 quand le slide en cours vaut l'index
+					key={index} // implementation of the picture with conditional display and opacity of 1 when the current picture is equal to the index
 					className={
 						current === index
 							? "current-picture"
@@ -34,26 +34,26 @@ export default function Slideshow({ slides }) {
 				</div>
 			))}
 			<div className='buttons-container'>
-              <div className="left-arrow">
-			{length > 1 && (
-				<img
-					src={left} //Affichage des flèches seulement si length > 1
-					alt="left arrow"
-					onClick={prevSlide}
-					className="left-arrow"
-				/>
-			)}
-            </div>
-            <div className="right-arrow">
-			{length > 1 && (
-				<img
-					src={right}
-					alt="right arrow"
-					onClick={nextSlide}
-					className="right-arrow"
-				/>
-			)}
-            </div>
+              	<div className="left-arrow">
+					{length > 1 && (
+						<img
+							src={left} //Display arrows only when length is greater than 1
+							alt="left arrow"
+							onClick={prevSlide}
+							className="left-arrow"
+						/>
+					)}
+            	</div>
+            	<div className="right-arrow">
+					{length > 1 && (
+						<img
+							src={right}
+							alt="right arrow"
+							onClick={nextSlide}
+							className="right-arrow"
+						/>
+					)}
+            	</div>
 			</div>
 		</section>
 	);
